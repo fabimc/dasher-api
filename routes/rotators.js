b74c3e4ed64d1721910e65e1960b4ddc4a6baed0
Fabián Montealegre <fabimc@gmail.com>
2019-12-23 16:03:46 -0600

    Updated rotators

--- routes/rotators.js
+++ routes/rotators.js
@@ -1,35 +1,34 @@
 var express = require("express");
+var R = require("ramda");
 var request = require("request-promise-native");
 var router = express.Router();
 
 /* GET rotators page. */
 router.get("/", function(req, res, next) {
-  var qs = req.query;
+  var qs = R.omit(["state"], req.query);
+  var user_state = R.path(["state"], R.pick(["state"], req.query)) || "logout";
   request({ url: "http://localhost:1337/rotators", qs: qs, json: true })
     .then(function(rotators) {
+      console.log("rotators[0].slides", rotators[0].slides);
       var slides = rotators[0].slides.map(function(slide) {
         return {
-          desktop_login: {
-            url: slide.desktopLogin ? slide.desktopLogin.url : ""
+          desktop: {
+            url: slide[`desktop_${user_state}`] ? slide[`desktop_${user_state}`].url : ""
           },
-          desktop_logout: {
-            url: slide.desktopLogout ? slide.desktopLogout.url : ""
+          mobile: {
+            url: slide[`mobile_${user_state}`] ? slide[`mobile_${user_state}`].url : ""
           },
-          mobile_login: {
-            url: slide.mobileLogin ? slide.mobileLogin.url : ""
-          },
-          mobile_logout: {
-            url: slide.mobileLogout ? slide.mobileLogout.url : ""
-          }
+          cta: slide[`cta_${user_state}`],
+          alt: slide.alt
         };
       });
+      
       console.log("slides", slides);
       res.render("rotator", { title: "Rotator", slides: slides });
     })
     .catch(function(err) {
       console.error(err);
     });
-  // res.render("rotator", { title: "Rotator", slides: slides });
 });
 
 module.exports = router;
